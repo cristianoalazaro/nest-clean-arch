d4df38e2ea8c99acbc316f70a60d7d955eda9ac5
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2025-01-11 08:20:48 -0300

    feat: Integration tests - validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -2,11 +2,12 @@ import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-buil'
 import { UserEntity, UserProps } from '../../user.entity'
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
+    let props: UserProps = {
+      ...UserDataBuilder({}),
+      name: null,
+    }
+
     it('Should throw an error when creating a user with invalid name', () => {
-      let props: UserProps = {
-        ...UserDataBuilder({}),
-        name: null,
-      }
       expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
 
       props = {
@@ -18,6 +19,44 @@ describe('UserEntity integration tests', () => {
       props = {
         ...UserDataBuilder({}),
         name: 'a'.repeat(256),
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 1 as any,
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+    })
+
+    it('Should throw an error when creating a user with invalid email', () => {
+      props = {
+        ...UserDataBuilder({}),
+        email: null,
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+
+      props = {
+        ...UserDataBuilder({}),
+        email: '',
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 'teste@teste',
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256),
+      }
+      expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 1 as any,
       }
       expect(() => new UserEntity(props)).toThrow('Entity Validation Error')
     })
