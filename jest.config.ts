6e1bd663bac4c6983a9e6a6399306f2e12c83a3b
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-04-09 20:11:54 -0300

    EnvConfigService unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
 import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from 'tsconfig.json'
+import { compilerOptions } from './tsconfig.json'
 
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
+  //rootDir: './',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testRegex: '.*\\..*spec\\ts$',
+  testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
