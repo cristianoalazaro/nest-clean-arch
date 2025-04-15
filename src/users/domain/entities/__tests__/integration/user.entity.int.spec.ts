5a610d1b4072aef324b64c556bc016a36596fc43
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-04-15 07:17:53 -0300

    Integration tests - validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -17,5 +17,22 @@ describe('UserEntity integration tests', () => {
       props = { ...UserDataBuilder({}), name: 10 as any }
       expect(() => new UserEntity(props)).toThrow(EntityValidationError)
     })
+
+    it('Should throw an error when create a user with invalid email', () => {
+      let props: UserProps = { ...UserDataBuilder({}), email: null as any }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = { ...UserDataBuilder({}), email: '' }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = { ...UserDataBuilder({}), email: 'test@test' }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = { ...UserDataBuilder({}), email: 'a'.repeat(101) }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = { ...UserDataBuilder({}), email: 10 as any }
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+    })
   })
 })
