9cf1a00ae899539b896b3eec2f28364dacf58803
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-04-22 19:38:19 -0300

    Adding search method  in InMemorySearchableRepository class

--- src/shared/infrastructure/env-config/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/infrastructure/env-config/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,49 @@
 import { Entity } from '../entities/entity'
 import { InMemoryRepository } from './in-memory.repository'
-import { SearchableRepository } from './searchable-repository-contract'
+import {
+  SearchableRepository,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contract'
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
-  implements SearchableRepository<E, any, any>
+  implements SearchableRepository<E, SearchParams, SearchResult<E>>
 {
-  search(props: any): Promise<any> {
-    throw new Error('Method not implemented.')
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const filteredItems = await this.applyFilter(this.items, props.filter)
+    const sortedItems = await this.applySort(
+      filteredItems,
+      props.sort,
+      props.sortDir,
+    )
+    const paginatedItems = await this.applyPaginate(
+      sortedItems,
+      props.page,
+      props.perPage,
+    )
+    return new SearchResult({
+      items: paginatedItems,
+      total: filteredItems.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    })
   }
+
+  protected async applyFilter(items: E[], filter: string | null): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
