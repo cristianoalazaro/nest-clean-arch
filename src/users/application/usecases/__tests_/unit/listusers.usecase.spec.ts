ec53e52616e97274ee9d3eb0ac8750d0412c7231
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-05-17 20:52:20 -0300

    ListUsersUseCase unit tests - part 2

--- src/users/application/usecases/__tests_/unit/listusers.usecase.spec.ts
+++ src/users/application/usecases/__tests_/unit/listusers.usecase.spec.ts
@@ -4,7 +4,7 @@ import { UserRepository } from 'src/users/domain/repositories/user.repository'
 import { UserEntity } from 'src/users/domain/entities/user.entity'
 import { UserDataBuilder } from 'src/users/domain/testing/helpers/user-data-builder'
 
-describe('ListUserUseCase unit tests', () => {
+describe('ListUsersUseCase unit tests', () => {
   let repository: UserInMemoryRepository
   let sut: ListUsersUseCase.UseCase
 
@@ -53,6 +53,55 @@ describe('ListUserUseCase unit tests', () => {
       total: 1,
       currentPage: 1,
       lastPage: 1,
+      perPage: 2,
+    })
+  })
+
+  it('Should return the users ordered by createdAt', async () => {
+    const createdAt = new Date()
+
+    const items = [
+      new UserEntity(UserDataBuilder({ createdAt })),
+      new UserEntity(
+        UserDataBuilder({ createdAt: new Date(createdAt.getTime() + 1) }),
+      ),
+    ]
+
+    repository.items = items
+    const output = await sut.execute({})
+
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(item => item.toJson()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    })
+  })
+
+  it('Should return the users using pagination, sort and filter', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+      new UserEntity(UserDataBuilder({ name: 'AA' })),
+      new UserEntity(UserDataBuilder({ name: 'Aa' })),
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+    ]
+
+    repository.items = items
+    const output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    })
+
+    expect(output).toStrictEqual({
+      items: [items[1].toJson(), items[2].toJson()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
       perPage: 2,
     })
   })
