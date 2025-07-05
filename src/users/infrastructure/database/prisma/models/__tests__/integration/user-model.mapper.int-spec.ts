27e595c63431d6778cea0948a0ba1ad6bb0a4d1b
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-07-05 18:47:46 -0300

    Adding forTest static method

--- src/users/infrastructure/database/prisma/models/__tests__/integration/user-model.mapper.int-spec.ts
+++ src/users/infrastructure/database/prisma/models/__tests__/integration/user-model.mapper.int-spec.ts
@@ -1,25 +1,24 @@
-import { execSync } from 'node:child_process'
 import { PrismaClient } from 'src/shared/infrastructure/database/generated/prisma'
+import { setupPrismaTests } from 'src/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
 import { UserModelMapper } from '../../user-model.mapper'
 import { ValidationError } from 'src/shared/infrastructure/domain/errors/validation-error'
 import { UserEntity } from 'src/users/domain/entities/user.entity'
-import { setupPrismaTests } from 'src/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
 
-describe('UserModelMapper integration tests', () => {
+describe('UserModelMapper', () => {
   jest.setTimeout(10_000)
 
   let prismaService: PrismaClient
   let props: any
 
-  beforeAll(async () => {
+  beforeAll(() => {
     setupPrismaTests()
-
     prismaService = new PrismaClient()
-    await prismaService.$connect()
+    prismaService.$connect()
   })
 
   beforeEach(async () => {
     await prismaService.user.deleteMany()
+
     props = {
       id: '2c0f8b60-8c3b-4bd8-b5d4-7954c00f79a5',
       name: 'Test Name',
@@ -30,9 +29,7 @@ describe('UserModelMapper integration tests', () => {
   })
 
   afterAll(async () => {
-    if (prismaService) {
-      await prismaService.$disconnect()
-    }
+    await prismaService.$disconnect()
   })
 
   it('Should throw an error when user model is invalid', async () => {
@@ -43,6 +40,7 @@ describe('UserModelMapper integration tests', () => {
   it('Should convert a user model to a user entity', async () => {
     const model = await prismaService.user.create({ data: props })
     const sut = UserModelMapper.toEntity(model)
+
     expect(sut).toBeInstanceOf(UserEntity)
     expect(sut.toJson()).toStrictEqual(props)
   })
