70a65ac7c6ac20a966f2976e97803752d6078567
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-12-06 18:21:42 -0300

    UserEntity - getters methods unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     props.createdAt = props.createdAt ?? new Date()
   }
+
+  get name() {
+    return this.name
+  }
+
+  get email() {
+    return this.email
+  }
+
+  get password() {
+    return this.password
+  }
+
+  get createdAt() {
+    return this.createdAt
+  }
 }
