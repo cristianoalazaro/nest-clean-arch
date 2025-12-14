f5c2237d050851ab7960c062e3586d272507c459
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-12-14 09:26:55 -0300

    ImMemorySearchableRepository unit tests - applyFilter method

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -25,13 +25,47 @@ class StubInMemorySearchableRepository extends InMemorySearchableRepository<Stub
 
 describe('InMemorySearchableRepository unit tests', () => {
   let sut: StubInMemorySearchableRepository
+  let items: StubEntity[]
 
   beforeEach(() => {
     sut = new StubInMemorySearchableRepository()
   })
 
-  describe('applyFilter', () => {})
+  describe('applyFilter', () => {
+    it('Should no filtered items when params is null', async () => {
+      items = [new StubEntity({ name: 'value name', price: 50 })]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+      const filteredItems = await sut['applyFilter'](items, null)
+
+      expect(filteredItems).toStrictEqual(items)
+      expect(spyFilterMethod).not.toHaveBeenCalled()
+    })
+
+    it('Should filter using filter value', async () => {
+      items = [
+        new StubEntity({ name: 'test', price: 50 }),
+        new StubEntity({ name: 'Test', price: 50 }),
+        new StubEntity({ name: 'fake', price: 50 }),
+      ]
+      const spyFilterMethod = jest.spyOn(items, 'filter')
+
+      let filteredItems = await sut['applyFilter'](items, 'TEST')
+      expect(filteredItems).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1)
+
+      filteredItems = await sut['applyFilter'](items, 'test')
+      expect(filteredItems).toStrictEqual([items[0], items[1]])
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2)
+
+      filteredItems = await sut['applyFilter'](items, 'no-filter')
+      expect(filteredItems).toHaveLength(0)
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+    })
+  })
+
   describe('applySort', () => {})
+
   describe('applyPagination', () => {})
+
   describe('search', () => {})
 })
--- src/users/infrastructure/database/repository/user-in-memory.repository.ts
+++ src/users/infrastructure/database/repository/user-in-memory.repository.ts
@@ -1,13 +1,20 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error'
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
-import { InMemorySearchRepository } from '@/shared/domain/repositories/in-memory-search.repository'
+import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserRepositoryInterface } from '@/users/repositories/user.repository.interface'
 
 export class UserInMemoryRepository
-  extends InMemorySearchRepository<UserEntity>
+  extends InMemorySearchableRepository<UserEntity>
   implements UserRepositoryInterface
 {
+  protected applyFilter(
+    items: UserEntity[],
+    filter: string | null,
+  ): Promise<UserEntity[]> {
+    throw new Error('Method not implemented.')
+  }
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email)
 
