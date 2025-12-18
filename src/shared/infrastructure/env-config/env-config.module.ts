ee79c6607462dbf33c9f6bffa71ae6c776d9b321
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-12-18 08:46:33 -0300

    Refactoring EnviConfigModule

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -4,7 +4,9 @@ import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { join } from 'node:path'
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
+  exports: [EnvConfigModule],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot<ValidationOptions extends Record<string, any>>(
@@ -13,10 +15,7 @@ export class EnvConfigModule extends ConfigModule {
     return super.forRoot(
       (options = {
         ...options,
-        envFilePath: join(
-          __dirname,
-          `../../../../.env.${process.env.NODE_ENV}`,
-        ),
+        envFilePath: join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
       }),
     )
   }
