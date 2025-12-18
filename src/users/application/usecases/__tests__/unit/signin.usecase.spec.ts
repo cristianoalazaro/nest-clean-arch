cd8e218d2d4b057bf16d8f002a0bd55f8396eef8
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-12-18 08:05:02 -0300

    Registering SignUpUseCase

--- src/users/application/usecases/__tests__/unit/signin.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/signin.usecase.spec.ts
@@ -1,8 +1,6 @@
-import { SignupUseCase } from '../../signup.usecase'
 import { UserInMemoryRepository } from '@/users/infrastructure/database/repository/user-in-memory.repository'
 import { BcryptHashProvider } from '@/users/infrastructure/providers/hashProvider/bcryptjs-hash.provider'
 import { UserDataBuilder } from '@/users/domain/entities/__tests__/testing/helpers/user-data-builder'
-import { ConflictError } from '@/shared/domain/errors/conflict-error'
 import { BadRequestError } from '@/shared/application/errors/bad-request-error'
 import { SigninUseCase } from '../../signin.usecase'
 import { UserEntity } from '@/users/domain/entities/user.entity'
--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { UsersController } from './users.controller'
+import { UserInMemoryRepository } from './database/repository/user-in-memory.repository'
+import { BcryptHashProvider } from './providers/hashProvider/bcryptjs-hash.provider'
+import { SignupUseCase } from '../application/usecases/signup.usecase'
+import { UserRepositoryInterface } from '../repositories/user.repository.interface'
+import { HashProvider } from '@/shared/application/providers/hash.provider'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptHashProvider,
+    },
+    {
+      provide: SignupUseCase.UseCase,
+      useFactory: (
+        userRepository: UserRepositoryInterface.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SignupUseCase.UseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
