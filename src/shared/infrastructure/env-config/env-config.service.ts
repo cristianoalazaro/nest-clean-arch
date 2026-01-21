34afe0cb06fd77dd69d09f64079d0b5174ce238c
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2026-01-21 07:41:14 -0300

    Refactoring git action

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -7,7 +7,8 @@ export class EnvConfigService implements EnvConfig {
   constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
-    return Number(this.configService.get<number>('PORT'))
+    const port = this.configService.get<string>('PORT')
+    return port ? Number(port) : 3000 // Retorna 3000 se não existir
   }
   getNodeEnv(): string {
     return this.configService.get<string>('NODE_ENV') ?? ''
