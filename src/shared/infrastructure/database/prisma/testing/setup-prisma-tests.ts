8a50048233da60af2eb670bc4290213b8ec013b5
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2026-01-25 11:27:34 -0300

    Refactoring database connection on tests

--- src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
+++ src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
@@ -1,5 +1,11 @@
 import { execSync } from 'node:child_process'
+import * as dotenv from 'dotenv'
+import { join } from 'node:path'
 
 export const setupPrismaTests = () => {
+  dotenv.config({ path: join(process.cwd(), '.env.test') })
   execSync('npx dotenv-cli -e .env.test -- npx prisma migrate deploy')
+
+  // Gera o client novamente para garantir que ele está atualizado com o schema do teste
+  execSync('npx prisma generate')
 }
