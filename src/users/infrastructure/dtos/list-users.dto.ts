794783352f261a63df8330fc5cfdf0121a990b4f
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2026-02-05 07:05:51 -0300

    Config ValidationPipe and DTOs

--- src/users/infrastructure/dtos/list-users.dto.ts
+++ src/users/infrastructure/dtos/list-users.dto.ts
@@ -1,10 +1,20 @@
 import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contract'
 import { ListUserUseCase } from '@/users/application/usecases/listUser.usecase'
+import { IsOptional } from 'class-validator'
 
 export class ListUsersDto implements ListUserUseCase.Input {
+  @IsOptional()
   page?: number
+
+  @IsOptional()
   perPage?: number
+
+  @IsOptional()
   sort?: string | null
+
+  @IsOptional()
   sortDir?: SortDirection | null
+
+  @IsOptional()
   filter?: string
 }
--- src/users/infrastructure/dtos/updatePassword-user.dto.ts
+++ src/users/infrastructure/dtos/updatePassword-user.dto.ts
@@ -1,6 +1,12 @@
 import { UpdatePasswordUserUseCase } from '@/users/application/usecases/updatePasswordUser.usecase'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class UpdatePasswordUserDto implements Omit<UpdatePasswordUserUseCase.Input, 'id'> {
+  @IsString()
+  @IsNotEmpty()
   password: string
+
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string
 }
